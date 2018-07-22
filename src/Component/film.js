280c425b6e8596f77bd534242d78a669dc551f6f
jnavb <jorgenavb@gmail.com>
2018-07-22 02:56:55 +0200

    Add UI rating and header

--- src/Component/film.js
+++ src/Component/film.js
@@ -5,33 +5,66 @@ export const Film = (({
   title, year, poster_big, sinopsis, director,
   script, rating, duration, cast
 }) => {
+  let colorBackground = {}
+  colorBackground = calculateBackground(rating)
   return (
-    <div className={styles.imdbPreview}>
-      <div className={styles.imdbHeader}>
-        <div className={styles.imdbTitle}>
-          <span>{title}</span>
-          <span className={styles.imdbYear}>({year})</span>
+    <div className={styles.preview}>
+      <div className={styles.header}>
+        <div className={styles.rating} style={colorBackground}>  
+          {rating ? <span>{rating}</span>
+                  : <span>N/A</span>}
         </div>
+        <span className={styles.title}>{title}</span>
+      </div>
+      <div className={styles.image}>
+        { poster_big && <img src={poster_big} className={styles.poster} /> }
       </div>
       <div className={styles.imdbMeta}>
-        { rating && <span>{rating}</span> }
-        { duration && <span>{duration}</span> }
+        { duration && <span>{duration} min.</span> }
         { year && <span>{year}</span> }
       </div>
-      <div className={styles.imdbMain}>
-        { poster_big && <img src={poster_big} className={styles.imdbPoster} /> }
-      </div>
+      <div>
       <dl className={styles.imdbDetails}>
-        <dt>Directors:</dt>
+        {director && <dt>Directors:</dt>}
         <dd>{director}</dd>
-        <dt>Writers:</dt>
+        {script && <dt>Writers:</dt>}
         <dd>{script}</dd>
-        <dt>Stars:</dt>
+        {cast && <dt>Stars:</dt>}
         <dd>{cast}</dd>
       </dl>
+      </div>
     </div>
     )
   }
 )
+
+const calculateBackground = (rating) => {
+  const ratingStyle = {}; 
+  if (rating >= 8) {
+    ratingStyle.background = 'ForestGreen';
+    ratingStyle['border-color'] = 'gold';
+  }
+  if (rating >= 7 && rating < 8) {
+    ratingStyle.background = 'ForestGreen';
+    ratingStyle['border-color'] = 'white';
+  }
+  if (rating >= 6 && rating < 7) {
+    ratingStyle.background = '#99cc00';
+    ratingStyle['border-color'] = 'white';
+  }
+  if (rating >= 5 && rating < 6) {
+    ratingStyle.background = '#cccc00';
+    ratingStyle['border-color'] = 'white';
+  }
+  if (rating >= 0 && rating < 5) {
+    ratingStyle.background = 'tomato';
+    ratingStyle['border-color'] = 'white';
+  }
+  if (!rating) {
+    ratingStyle .background = 'grey';
+    ratingStyle['border-color'] = 'white';
+  }
+  return ratingStyle;
+}
 
 export default Film;
--- src/Container/detailFilm.js
+++ src/Container/detailFilm.js
@@ -7,18 +7,27 @@ export class Prev extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null
+      detailFilm: null
     }
   }
   componentDidMount() {
     const { id } = this.props;
-    detailFilm(id).then(data => this.setState({ data }))
+    detailFilm(id).then(data => {
+      const detailFilm = transformDetailFilm(data);
+      this.setState({ detailFilm });
+    })
   }
   render() {
-    const { data } = this.state;
-    return (data) ? <div><Film {...data} /></div>
-                  : <div><Loading /></div>
+    const { detailFilm } = this.state;
+    return (detailFilm) ? <Film {...detailFilm}/>
+                  : <Loading />
   }
+}
+const transformDetailFilm = (detailFilmToParse) => {
+  const detailFilm = detailFilmToParse;
+  if (Array.isArray(detailFilm.cast)) 
+    detailFilm.cast = detailFilm.cast.slice(0,6).join(' ');
+  return detailFilm;
 }
 
 export default Prev;
